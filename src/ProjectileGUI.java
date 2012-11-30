44f4c9e6a7d4d4276fc0152147172f629bb92eb5
Timm Nygren <tnygren@mines.edu>
2012-11-30 16:58:57 -0700

    Adding JFrame of game

---
+++ src/ProjectileGUI.java
@@ -0,0 +1,48 @@
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+
+
+public class ProjectileGUI extends JFrame{
+	
+	public ProjectileGUI() {
+		//add FProjMain panel
+		
+		
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		menuBar.add(createFileMenu());
+		
+		setSize(new Dimension(800, 800));
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setTitle("Projectile Game");
+	}
+	
+	private JMenu createFileMenu() {
+		JMenu menu = new JMenu("File");
+		menu.add(createFileExitItem());
+		return menu;
+	}
+	
+	private JMenuItem createFileExitItem() {
+		JMenuItem item = new JMenuItem("Exit");
+		class MenuItemListener implements ActionListener {
+			public void actionPerformed(ActionEvent e)
+			{
+				System.exit(0);
+			}
+		}
+		item.addActionListener(new MenuItemListener());
+		return item;
+	}
+	
+	public static void main(String[] args) {
+		ProjectileGUI projectileGame = new ProjectileGUI();
+		projectileGame.setVisible(true);
+	}
+}
